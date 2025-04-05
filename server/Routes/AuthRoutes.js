93defc2e945527fc5d214ef62b2a023ed76fc230
lalit <lkjlalitkumar2005@gmail.com>
2025-04-05 23:12:04 +0530

    Production

--- server/Routes/AuthRoutes.js
+++ server/Routes/AuthRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { loginUser, registerUser } from '../controllers/authController';
+import { loginUser, registerUser } from '../controllers/AuthController.js';
 
 
 const AuthRouter = express.Router();
