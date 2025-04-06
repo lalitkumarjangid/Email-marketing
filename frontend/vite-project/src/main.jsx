dc61b84f9bd7aac28a5b01712558ca0bd1959b58
lalit <lkjlalitkumar2005@gmail.com>
2025-04-06 13:45:46 +0530

    Added vercel.json

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";  // Make sure this path is correct
+import { store } from "./redux/store";  
 import App from "./App.jsx";
 import "./index.css";
 
